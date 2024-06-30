7f3911ba5e2ba19534e1a51388735ff0806f4510
Gustavo Petta <160749630+PettaDev@users.noreply.github.com>
2024-06-30 13:14:02 -0300

    Update Hospital_Parte004.sql

--- ModeloFísico/Hospital_Parte004.sql
+++ ModeloFísico/Hospital_Parte004.sql
@@ -2,3 +2,12 @@
 
 # Crie um script para atualizar ao menos dois médicos como inativos e os demais em atividade.
 
+-- Adicionar a coluna em_atividade na tabela medicos definindo (true 1 para todos)
+ALTER TABLE medicos ADD COLUMN em_atividade BOOLEAN NOT NULL DEFAULT TRUE;
+
+-- Atualizar o status de atividade dos médicos
+-- Pelo menos dois medicos como inativos (false)
+UPDATE medicos SET em_atividade = FALSE WHERE medico_id IN (1, 2);
+
+-- Atualizar os demais médicos como em atividade (true)
+UPDATE medicos SET em_atividade = TRUE WHERE medico_id NOT IN (1, 2);
